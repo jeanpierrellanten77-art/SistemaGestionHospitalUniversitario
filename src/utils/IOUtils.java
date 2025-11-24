31f500b4cf0b50ada3ea7b89ba21fcc4dfab27d8
jeanpierrellanten77-art <jeanpierrellanten77@gmail.com>
2025-11-23 20:48:50 -0500

    Delete src/utils/IOUtils.java

--- src/utils/IOUtils.java
+++
@@ -1,8 +0,0 @@
-public class IOUtils {
-
-    // Constructor privado para evitar instanciación
-    private IOUtils() {}
-
-    public static void guardarDatos(Object datos, String archivo) {}
-    public static Object cargarDatos(String archivo) { return null; }
-}
