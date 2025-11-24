70343df0d953355f45e598719f860c9a70b8d8df
jeanpierrellanten77-art <jeanpierrellanten77@gmail.com>
2025-11-24 02:43:23 -0500

    Add files via upload

--- src/Main/resources/utp/Ac/Pa/Sistema/utils/IOUtils.java
+++ src/Main/resources/utp/Ac/Pa/Sistema/utils/IOUtils.java
@@ -1,1 +1,20 @@
+package utp.ac.pa.sistema.utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class IOUtils {
+    public static void saveText(String filename, String content) {
+        Path p = Paths.get(filename);
+        try { Files.write(p, content.getBytes()); }
+        catch (IOException e) { }
+    }
+
+    public static String readText(String filename) {
+        Path p = Paths.get(filename);
+        try { return new String(Files.readAllBytes(p)); }
+        catch (IOException e) { return null; }
+    }
+}
